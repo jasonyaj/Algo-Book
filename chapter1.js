007b31e89670e899a49a4760eec306e5754c3537
jasonyaj <jason.yaj@gmail.com>
2023-03-28 12:30:26 -0500

    added more work, Buggy13 and w3d2 morning algo

--- chapter1.js
+++ chapter1.js
@@ -70,39 +70,39 @@
 // // Implement generateCoinChange(cents)​ that accepts a parameter for the number of cents, and
 // // computes how to represent that amount with the smallest number of coins. Console.log the result.
 // var coins = []; // create variable = [store coin type and count in here]
-var quarter = 0;
-var dime = 0;
-var nickel = 0;
-var penny = 0;
-var remainder = 0;
-function generateCoinChange(cents) {
-  console.log('cents: ' + cents);
-  for (var i = cents; i > 0; i = remainder) {
-    if (i > 24) {
-      quarter = Math.floor(i / 25); //log the integer into quarter
-      remainder = i % 25; //take the remainder loop it back up to check if still greater then 0
-      // console.log("remainder: " + remainder);
-    } else if (i < 25 && i > 9) {
-      dime = Math.floor(i / 10);
-      remainder = remainder % 10;
-      // console.log("remainder: " + remainder);
-    } else if (i < 10 && i > 4) {
-      nickel = Math.floor(i / 5);
-      remainder = remainder % 5;
-      // console.log("remainder: " + remainder);
-    } else {
-      penny = i / 1;
-      remainder = 0;
-      // console.log("remainder: " + remainder);
-      // console.log("remainder: " + remainder);
-    }
-  }
-  console.log(quarter + " quarter(s)");
-  console.log(dime + " dime(s)");
-  console.log(nickel + " nickel(s)");
-  console.log(penny + " penny(s)");
-}
-generateCoinChange((Math.floor(Math.random()*100)));
+// var quarter = 0;
+// var dime = 0;
+// var nickel = 0;
+// var penny = 0;
+// var remainder = 0;
+// function generateCoinChange(cents) {
+//   console.log('cents: ' + cents);
+//   for (var i = cents; i > 0; i = remainder) {
+//     if (i > 24) {
+//       quarter = Math.floor(i / 25); //log the integer into quarter
+//       remainder = i % 25; //take the remainder loop it back up to check if still greater then 0
+//       // console.log("remainder: " + remainder);
+//     } else if (i < 25 && i > 9) {
+//       dime = Math.floor(i / 10);
+//       remainder = remainder % 10;
+//       // console.log("remainder: " + remainder);
+//     } else if (i < 10 && i > 4) {
+//       nickel = Math.floor(i / 5);
+//       remainder = remainder % 5;
+//       // console.log("remainder: " + remainder);
+//     } else {
+//       penny = i / 1;
+//       remainder = 0;
+//       // console.log("remainder: " + remainder);
+//       // console.log("remainder: " + remainder);
+//     }
+//   }
+//   console.log(quarter + " quarter(s)");
+//   console.log(dime + " dime(s)");
+//   console.log(nickel + " nickel(s)");
+//   console.log(penny + " penny(s)");
+// }
+// generateCoinChange((Math.floor(Math.random()*100)));
 
 /**Statistics to Doubles
 Implement a ‘die’ that randomly returns an
