1048f7ea8ddb0777269fff88e22825a91663ee5e
jasonyaj <jason.yaj@gmail.com>
2023-03-29 19:53:05 -0500

    added cards mjs and w3d3 mjs

---
+++ w3d3.mjs
@@ -0,0 +1,35 @@
+import { suits, ranks, emojis } from './cards.mjs';
+
+/* 
+  Deck of Cards
+
+  From the imported arrays, create a function that
+  builds a deck of cards. The deck will be a one-
+  dimensional array of objects, such that each object
+  represents one card in the deck.
+
+  Example card: { suit: 'hearts', rank: '2' };
+  
+  Bonus: Use the included emoji matrix array to add
+  a third property to each card.
+
+  Example card with emoji property:
+  { suit: 'hearts', rank: '2', emoji: '🂲' };
+*/
+
+function buildDeck(suits, ranks) {
+  // your code here
+}
+
+/* 
+  Shuffle
+  
+  Using the built deck, create a shuffle function
+  implementing the Fisher-Yates shuffle algorithm.
+  Return the deck after the shuffle.
+  see: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+*/
+
+function shuffleDeck(deck) {
+  // your code here
+}
