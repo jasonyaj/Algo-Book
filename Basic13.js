acf001da2bdb58605c8ace2d10565a5a17699db4
jasonyaj <jason.yaj@gmail.com>
2023-04-02 11:17:05 -0500

    reorganized Basic13 order

--- Basic13.js
+++ Basic13.js
@@ -4,8 +4,20 @@
 // Print Sum 0-255
 // Print integers from 0 to 255, and with each integer print the sum so far.
 
+// Print Odds 1-255
+// Print all odd integers from 1 to 255.
+
+// Iterate and Print Array
+// Iterate through a given array, printing each value.
+
 // Find and Print Max
 // Given an array, find and print its largest element.
+
+// Get and Print Average
+// Analyze an array’s values and print the average.
+
+// Max, Min, Average
+// Given an array, print the max, min and average values for that array.
 
 // Array with Odds
 // Create an array with all the odd integers between 1 and 255 (inclusive).
@@ -13,26 +25,14 @@
 // Greater Than Y
 // Given an array and a value Y, count and print the number of array values greater than Y.
 
-// Max, Min, Average
-// Given an array, print the max, min and average values for that array.
-
 // Swap String For Array Negative Values
 // Replace any negative array values with 'Dojo'​.
 
-// Print Odds 1-255
-// Print all odd integers from 1 to 255.
-
-// Iterate and Print Array
-// Iterate through a given array, printing each value.
-
-// Get and Print Average
-// Analyze an array’s values and print the average.
+// Zero Out Negative Numbers
+// Return the given array, after setting any negative values to zero.
 
 // Square the Values
 // Square each value in a given array, returning that same array with changed values.
-
-// Zero Out Negative Numbers
-// Return the given array, after setting any negative values to zero.
 
 // Shift Array Values
 // Given an array, move all values forward by one index, dropping the first and leaving a '0'​ value at the
